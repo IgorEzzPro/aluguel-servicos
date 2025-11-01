f67a4edf0bc7f068c187a347b60c9751c582aed0
IgorEzzPro <staropticaprovider@gmail.com>
2025-10-31 23:02:32 -0300

    Delete aluguel-servicos directory

--- aluguel-servicos/perfil/profissional.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("valorServico").textContent = localStorage.getItem("valorServico") || "N/A";
-
-function contratar() {
-  window.location.href = "../pagamento/pagamento.html";
-}
-document.getElementById("contratarBtn").addEventListener("click", contratar);
-// perfil/profissional.js
